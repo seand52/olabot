ac766e7929c21e728fea317be0294966c07ac10f
Sean <seand52@gmail.com>
2023-02-04 20:00:01 +0000

    add rate limiter better

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -21,27 +22,41 @@ func main() {
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
+	u.AllowedUpdates = []string{"message"}
+	// don't make the bot inline bot
 
 	updates := bot.GetUpdatesChan(u)
 	counter := 0
 	reset := time.Tick(time.Minute)
+
 	for update := range updates {
 		if update.Message != nil {
-			if update.Message.Text == "ola" {
-				counter++
-				if counter > 5 {
+				select {
+				case <-reset:
 					counter = 0
-					<-reset
-				} else {
-					log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "ola")
-					msg.ReplyToMessageID = update.Message.MessageID
-
-					bot.Send(msg)
+				default:
+					log.Printf("counter is %d", counter)
+					if counter < 5 {
+						if 	strings.ToLower(update.Message.Text) == "ola" {
+							counter++
+							if counter > 5 {
+								counter = 0
+								<-reset
+							} else {
+								log.Printf("hello [%s] %s", update.Message.From.UserName, update.Message.Text)
+			
+								msg := tgbotapi.NewMessage(update.Message.Chat.ID, "ola")
+								msg.ReplyToMessageID = update.Message.MessageID
+			
+								bot.Send(msg)
+							}
+			
+						}
+					} else {
+						log.Printf("Rate limit reached. Try again in %s", time.Until(time.Now().Truncate(time.Minute).Add(time.Minute)))
+					}
 				}
-
-			}
+			
 		}
 	}
 }
